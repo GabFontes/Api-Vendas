84225998da826e125268db4ad4e7fd16083a7305
gab <gabrielfontes321@gmail.com>
2022-06-21 13:26:34 -0300

    feat: implementing ListProductService

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,13 @@
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
+
+export default class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const products = await productsRepository.find();
+
+    return products;
+  }
+}
