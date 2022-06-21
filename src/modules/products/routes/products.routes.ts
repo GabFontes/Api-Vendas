6d621c626b797da2dc66a60c50ba199faeae88e9
gab <gabrielfontes321@gmail.com>
2022-06-21 13:25:50 -0300

    feat: implementing ProductsRouter

---
+++ src/modules/products/routes/products.routes.ts
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+import ProductsController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
+
+const productsRouter = Router();
+const productsController = new ProductsController();
+
+productsRouter.get('/', productsController.list);
+
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.create,
+);
+
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.show,
+);
+
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.update,
+);
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.delete,
+);
+
+export default productsRouter;
