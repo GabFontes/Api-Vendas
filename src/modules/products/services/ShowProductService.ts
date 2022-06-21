ef363d84a2edc84335d5bdd0f9661ccbfd89facc
gab <gabrielfontes321@gmail.com>
2022-06-21 13:26:49 -0300

    feat: implementing ShowProductService

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+export default class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+    // Verfica se o produto existe antes de exibi-lo.
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    return product;
+  }
+}
