d124ecc9642be87321b7971a4e4bc8f515859e4d
gab <gabrielfontes321@gmail.com>
2022-06-21 13:29:15 -0300

    feat: implementing CreateUserService

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+
+    // Verifica se existe um usuário com o mesmo email antes de criá-lo
+    if (emailExists) {
+      throw new AppError('Email addres already used');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
