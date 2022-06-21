3cbdbd1754fc6a3e185549a23cb954f9b2aca994
gab <gabrielfontes321@gmail.com>
2022-06-21 13:23:34 -0300

    fix: fixing incorrect request parameter in error middleware and importing celebrate errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, response, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -16,19 +17,25 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
+app.use(errors());
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error',
-  });
-});
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(error.message);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
