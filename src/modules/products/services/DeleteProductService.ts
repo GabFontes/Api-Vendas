73359fa679dc8119323c0de89e3a12c30af30807
gab <gabrielfontes321@gmail.com>
2022-06-21 13:27:05 -0300

    feat: implementing DeleteProductService

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+export default class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+    // Verfica se o produto existe antes de exibi-lo.
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    await productsRepository.remove(product);
+  }
+}
