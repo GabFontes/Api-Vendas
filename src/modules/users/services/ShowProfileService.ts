6d90d3123c2332d8bc8b3aa898acda72ccd3b465
gab <gabrielfontes321@gmail.com>
2022-07-02 19:45:24 -0300

    feat(src/modules/users/services): creating ShowProfileService and UpdateProfileService

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+export default class ShowProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user;
+  }
+}
