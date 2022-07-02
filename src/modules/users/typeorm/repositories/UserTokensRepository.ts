ad293338f8a8b133ffe3c935ba44b22add5df448
gab <gabrielfontes321@gmail.com>
2022-07-02 19:44:31 -0300

    feat(src/modules/users/typeorm): creating UserToken Entitiy and UserTokensRepository Repository

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,25 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+export default class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = await this.create({
+      user_id,
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  }
+}
